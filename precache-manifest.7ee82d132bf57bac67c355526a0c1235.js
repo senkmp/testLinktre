b4b6f5ad43326a8eb79c74401973bd216e223a88
senkmp <senkmp@gmail.com>
2021-10-31 14:43:40 +0530

    Updates

---
+++ precache-manifest.7ee82d132bf57bac67c355526a0c1235.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83bd02281fc0bddb750a03d7ec6df439",
+    "url": "/testLinktre/index.html"
+  },
+  {
+    "revision": "95ac038829b03d8434d1",
+    "url": "/testLinktre/static/css/main.6b7e6472.chunk.css"
+  },
+  {
+    "revision": "e20b52eaa6ffd6d0b14c",
+    "url": "/testLinktre/static/js/2.d84aa850.chunk.js"
+  },
+  {
+    "revision": "95ac038829b03d8434d1",
+    "url": "/testLinktre/static/js/main.c770c4fe.chunk.js"
+  },
+  {
+    "revision": "fcc53d183e016abdeadb",
+    "url": "/testLinktre/static/js/runtime-main.c9f7392b.js"
+  }
+]);
